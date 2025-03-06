a8efea509a4b3ff286a8235305a74abf7a9488b8
Gorazd <fi.gorazd@gmail.com>
2025-03-06 19:08:50 +0100

    add claimhub endpoints (not tested)

--- backend/claimhub/urls.py
+++ backend/claimhub/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import ServiceListView, ProductListView, PurchaseItemView, ItemDetailView
+from .views import ServiceListView, ProductListView, PurchaseItemView, ItemDetailView, GrantCreateView,GrantListView,GrantDetailView,ApplyGrantView
 
 urlpatterns = [
-    path('services/', ServiceListView.as_view(), name='services-list'),
+    path('services/', ServiceListView.as_view(), name='services-list'), #ok
     path('products/', ProductListView.as_view(), name='products-list'),
     path('purchase/', PurchaseItemView.as_view(), name='purchase-item'),
     path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
+    path('grants/', GrantListView.as_view(), name='grant-list'),
+    path('grants/create/<int:company_id>/', GrantCreateView.as_view(), name='grant-create'),
+    path('grants/<int:pk>/', GrantDetailView.as_view(), name='grant-detail'),
+    path('grants/<int:grant_id>/apply/<int:user_id>/', ApplyGrantView.as_view(), name='grant-apply'),
 ]
